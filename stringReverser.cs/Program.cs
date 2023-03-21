63ede78e46f377858551821733222e4d0365c9f2
Andressa Guedes <andressajuliagr@hotmail.com>
2023-03-21 17:48:07 -0300

    Adicionar arquivos de projeto.

---
+++ stringReverser.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma palavra a ser invertida:");
+        string str = Console.ReadLine();
+        char[] arr = str.ToCharArray();
+
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            char temp = arr[i];
+            arr[i] = arr[arr.Length - i - 1];
+            arr[arr.Length - i - 1] = temp;
+        }
+
+        string strInvertida = new string(arr);
+        Console.WriteLine(strInvertida);
+    }
+}
+
